79f2b93d73c9a17596a9fca6c6121518c0a45883
Nick Salvatore <nmsalvatore@gmail.com>
2025-10-04 09:07:51 -0700

    refactor: better Repeat description

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -2,7 +2,7 @@ package iteration
 
 import "strings"
 
-// Repeat builds a string by repeating a character a specified number of times
+// Repeat builds a string by repeating a character a given number of times.
 func Repeat(character string, repeatCount int) string {
 	var repeated strings.Builder
 	for range repeatCount {
