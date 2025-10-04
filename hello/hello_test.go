439826e6ea16d054fd9a982c1838b3fa6a986947
Nick Salvatore <nmsalvatore@gmail.com>
2025-10-03 17:45:11 -0700

    refactor: create helper function to assert error message in tests

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,9 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Nick")
-	want := "Hello, Nick!"
+	t.Run("saying hello to someone", func(t *testing.T) {
+		got := Hello("Nick")
+		want := "Hello, Nick!"
+		assertCorrectMessage(t, got, want)
+	})
 
+	t.Run("say hello to world if no name supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world!"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
