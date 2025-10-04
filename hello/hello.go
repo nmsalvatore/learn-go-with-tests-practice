ff4cf7c88995f489a2ece6adf9ffccf8e898cd47
Nick Salvatore <nmsalvatore@gmail.com>
2025-10-03 17:57:17 -0700

    feat: added french translation

--- hello/hello.go
+++ hello/hello.go
@@ -2,15 +2,23 @@ package main
 
 import "fmt"
 
+const french = "French"
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
+const frenchHelloPrefix = "Bonjour, "
+const spanishHelloPrefix = "Hola, "
 
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "world"
 	}
 
-	if language == "Spanish" {
-		return "Hola, " + name + "!"
+	if language == french {
+		return frenchHelloPrefix + name + "!"
+	}
+
+	if language == spanish {
+		return spanishHelloPrefix + name + "!"
 	}
 	return englishHelloPrefix + name + "!"
 }
