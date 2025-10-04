1d058b7dea019da5e3f95cc965644362b155dfa6
Nick Salvatore <nmsalvatore@gmail.com>
2025-10-04 10:17:45 -0700

    feat: check for empty slices, create test helper checkSums

--- arrays-slices/sum.go
+++ arrays-slices/sum.go
@@ -9,15 +9,23 @@ func Sum(numbers []int) (sum int) {
 
 func SumAll(allNumbers ...[]int) (sum []int) {
 	for _, numbers := range allNumbers {
-		sum = append(sum, Sum(numbers))
+		if len(numbers) == 0 {
+			sum = append(sum, 0)
+		} else {
+			sum = append(sum, Sum(numbers))
+		}
 	}
 	return
 }
 
 func SumAllTails(allNumbers ...[]int) (sum []int) {
 	for _, numbers := range allNumbers {
-		tail := numbers[1:]
-		sum = append(sum, Sum(tail))
+		if len(numbers) == 0 {
+			sum = append(sum, 0)
+		} else {
+			tail := numbers[1:]
+			sum = append(sum, Sum(tail))
+		}
 	}
 	return
 }
