1a5c3b5e505a1758d2187ecb526bfac582c6d0b1
Nick Salvatore <nmsalvatore@gmail.com>
2025-10-05 12:48:50 -0700

    feat: check if word already exists on Add method

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -23,20 +23,38 @@ func TestSearch(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	word := "test"
-	definition := "this is just a test"
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
 
-	dictionary.Add(word, definition)
+		dictionary.Add(word, definition)
 
-	want := "this is just a test"
-	got, err := dictionary.Search("test")
+		assertDefinition(t, dictionary, word, definition)
+
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, definition)
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
+}
+
+func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
 
 	if err != nil {
 		t.Fatal("should find added word:", err)
 	}
 
-	assertStrings(t, got, want)
+	assertStrings(t, got, definition)
 }
 
 func assertError(t testing.TB, got, want error) {
