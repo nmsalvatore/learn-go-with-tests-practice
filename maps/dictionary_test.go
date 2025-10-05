63148e42390e522e9b60c1da0cb761965f1242cd
Nick Salvatore <nmsalvatore@gmail.com>
2025-10-05 12:04:34 -0700

    feat: add Add method on Dictionary

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -22,6 +22,20 @@ func TestSearch(t *testing.T) {
 	})
 }
 
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	dictionary.Add("test", "this is just a test")
+
+	want := "this is just a test"
+	got, err := dictionary.Search("test")
+
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	assertStrings(t, got, want)
+}
+
 func assertError(t testing.TB, got, want error) {
 	t.Helper()
 
