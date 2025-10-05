5ef7e540d5acb4abca90150a6c86809c164900d2
Nick Salvatore <nmsalvatore@gmail.com>
2025-10-05 10:43:16 -0700

    refactor: check for no error on withdraw success

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -13,7 +13,9 @@ func TestWallet(t *testing.T) {
 
 	t.Run("withdraw bitcoin", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-		wallet.Withdraw(Bitcoin(8))
+		err := wallet.Withdraw(Bitcoin(8))
+
+		assertNoError(t, err)
 		assertBalance(t, wallet, Bitcoin(12))
 	})
 
@@ -29,10 +31,19 @@ func TestWallet(t *testing.T) {
 
 func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
 	t.Helper()
+
 	got := wallet.balance
 
 	if got != want {
 		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
 	}
 }
 
