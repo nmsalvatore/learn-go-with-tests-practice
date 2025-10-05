2c4a1119f453f791a345847eef28d03f0c0fcf7e
Nick Salvatore <nmsalvatore@gmail.com>
2025-10-05 13:17:47 -0700

    feat: check if word exists on Update

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -41,19 +41,30 @@ func TestAdd(t *testing.T) {
 		err := dictionary.Add(word, definition)
 
 		assertError(t, err, ErrWordExists)
-		assertDefinition(t, dictionary, word, definition)
 	})
 }
 
 func TestUpdate(t *testing.T) {
-	word := "test"
-	definition := "this is just a test"
-	dictionary := Dictionary{word: definition}
-	newDefinition := "new definition"
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		newDefinition := "new definition"
 
-	dictionary.Update(word, newDefinition)
+		dictionary.Update(word, newDefinition)
 
-	assertDefinition(t, dictionary, word, newDefinition)
+		assertDefinition(t, dictionary, word, newDefinition)
+	})
+
+	t.Run("new word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{}
+
+		err := dictionary.Update(word, definition)
+
+		assertError(t, err, ErrWordDoesNotExist)
+	})
 }
 
 func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
