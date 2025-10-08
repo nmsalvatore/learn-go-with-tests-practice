8d59618297984cc8d1d47c473d947c9d5204d3e8
Nick Salvatore <nmsalvatore@gmail.com>
2025-10-07 20:03:19 -0700

    feat: completed dependency injection

---
+++ mocking/countdown.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const countdownStart = 3
+const finalWord = "Go!"
+
+func Countdown(out io.Writer) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		time.Sleep(1 * time.Second)
+	}
+
+	fmt.Fprint(out, finalWord)
+}
+
+func main() {
+	Countdown(os.Stdout)
+}
