8ca6208af278bf19f7d6fa2283d9ab1df6e17ae5
Nick Salvatore <nmsalvatore@gmail.com>
2025-10-12 10:05:27 -0700

    feat: half-baked reflection code

---
+++ reflection/reflection.go
@@ -0,0 +1,15 @@
+package reflection
+
+import "reflect"
+
+func walk(x any, fn func(input string)) {
+	val := reflect.ValueOf(x)
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+
+		if field.Kind() == reflect.String {
+			fn(field.String())
+		}
+	}
+}
